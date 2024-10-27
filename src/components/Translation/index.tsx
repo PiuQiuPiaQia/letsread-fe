e8b819353e60d1f82caab60e910c9bf2a1760da1
Mario <329988300@qq.com>
2024-10-27 23:55:00 +0800

    增加翻译

--- src/components/Translation/index.tsx
+++ src/components/Translation/index.tsx
@@ -2,22 +2,40 @@ import { ConnectState } from "@/models/connect";
 import { storeContainer } from "@/pages/DashboardCommon/RobotStruct/store";
 import { useSelector } from "umi";
 import styles from "./index.less";
+import { Button, Spin } from "antd";
+import { useState } from "react";
+import Loading from "../Loading";
 
 export default function Translation() {
+  const [hasContent, setHasContent] = useState(false);
+  const [translations, setTranslations] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
   const { resultJson } = storeContainer.useContainer();
-  let translations: any[] = [];
+  let original: any[] = [];
   console.log(resultJson);
   if (resultJson) {
+    !hasContent && setHasContent(true);
     const pages = resultJson.pages;
-    translations = pages.map((page) => {
+    original = pages.map((page) => {
       const content = page.content;
       return content.map((item) => item.text || "").join("");
     });
     console.log("pages:", pages);
   }
 
+  const handleTranslate = () => {
+    setLoading(true);
+    setTranslations([]);
+    setTimeout(() => {
+      setTranslations(original);
+      setLoading(false);
+    }, 1000);
+  };
+
   return (
     <div className={styles.translation}>
+      {loading && <Loading type="loading" text="翻译中。。。" />}
+      {hasContent ? <Button onClick={handleTranslate}>开始翻译</Button> : null}
       {translations.map((item, index) => (
         <div className={styles.translation__item} key={index}>
           {item}
