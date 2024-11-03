49c98afc3fd63c3cafa3ee179214d4986c8b9144
Mario <329988300@qq.com>
2024-11-03 14:04:02 +0800

    add tinymce

--- config/config.ts
+++ config/config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'umi';
-import theme from '../src/utils/theme';
-import defaultSettings from './defaultSettings';
-import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
-import { prefixPath } from '../src/utils/env';
-import path from 'path';
-const esbuild = require('esbuild');
+import { defineConfig } from "umi";
+import theme from "../src/utils/theme";
+import defaultSettings from "./defaultSettings";
+import MonacoWebpackPlugin from "monaco-editor-webpack-plugin";
+import { prefixPath } from "../src/utils/env";
+import path from "path";
+const esbuild = require("esbuild");
 const { NODE_ENV } = process.env;
 
 /**
@@ -12,7 +12,7 @@ const { NODE_ENV } = process.env;
  */
 export default defineConfig({
   history: {
-    type: 'hash',
+    type: "hash",
   },
   define: {
     env: NODE_ENV,
@@ -23,12 +23,12 @@ export default defineConfig({
   publicPath: prefixPath,
   ignoreMomentLocale: true,
   theme: {
-    'primary-color': defaultSettings.primaryColor,
-    'primary-color-hover': '#1A66FF',
-    'border-color-base': '#DCDFE5',
-    'text-color': '#030A1A',
-    'normal-icon-color': '#858C99',
-    'normal-font-color': '#030A1A',
+    "primary-color": defaultSettings.primaryColor,
+    "primary-color-hover": "#1A66FF",
+    "border-color-base": "#DCDFE5",
+    "text-color": "#030A1A",
+    "normal-icon-color": "#858C99",
+    "normal-font-color": "#030A1A",
     ...theme,
   },
   antd: {},
@@ -41,33 +41,34 @@ export default defineConfig({
     // firefox: 52,
   },
   dynamicImport: {
-    loading: '@/pages/Loading',
+    loading: "@/pages/Loading",
   },
   dynamicImportSyntax: {},
   headScripts: [
     { src: `${prefixPath}classList.polyfill.min.js`, defer: true },
     { src: `${prefixPath}xlsx.full.min.js`, defer: true },
+    { src: `${prefixPath}tinymce/tinymce.min.js`, defer: true },
   ],
   externals: {
     // react: 'window.React',
-    xlsx: 'XLSX',
+    xlsx: "XLSX",
   },
   scripts: [],
-  copy: ['./vendor/classList.polyfill.min.js'],
+  copy: ["./vendor/classList.polyfill.min.js"],
   cssLoader: {
     modules: {
-      localIdentName: '[local]--[hash:base64:5]',
+      localIdentName: "[local]--[hash:base64:5]",
       getLocalIdent: (
         context: {
           resourcePath: string;
         },
         _: string,
-        localName: string,
+        localName: string
       ) => {
         if (
-          context.resourcePath.includes('node_modules') ||
-          context.resourcePath.includes('ant.design.pro.less') ||
-          context.resourcePath.includes('global.less')
+          context.resourcePath.includes("node_modules") ||
+          context.resourcePath.includes("ant.design.pro.less") ||
+          context.resourcePath.includes("global.less")
         ) {
           return localName;
         }
@@ -85,25 +86,16 @@ export default defineConfig({
   manifest: {
     basePath: prefixPath,
   },
-  metas: [
-    {
-      name: 'keywords',
-      content:
-        'OCR，深度学习，AI，文字识别，图像识别，表格识别，身份证识别，发票识别，银行卡识别，营业执照识别，图像技术，在线免费识别',
-    },
-    {
-      name: 'description',
-      content:
-        'TextIn 合合信息旗下智能文字识别品牌，专注智能文字识别15年，为扫描全能王、名片全能王提供OCR能力，是集智能文字识别技术、产品、服务于一身的一站式OCR服务平台，为全球数千家企业及上亿用户提供服务。',
-    },
-  ],
+  metas: [],
   alias: {
-    '@root': '/',
+    "@root": "/",
   },
   //["module-resolver", { root:["./"] ,"alias": { "@": path.resolve(__dirname,"../src") } }],
   // extraBabelPlugins:[['inline-react-svg',{}],["module-resolver", { root:["../"] ,"alias": { "@": "./src" } }]],
-  extraBabelIncludes: [path.join(__dirname, './node_modules/exif-rotate-js/lib')],
-  chunks: ['vendors', 'umi'],
+  extraBabelIncludes: [
+    path.join(__dirname, "./node_modules/exif-rotate-js/lib"),
+  ],
+  chunks: ["vendors", "umi"],
   chainWebpack(config) {
     config.merge({
       optimization: {
@@ -111,35 +103,35 @@ export default defineConfig({
           cacheGroups: {
             commons: {
               test: /(react|react-dom)/,
-              name: 'vendors',
-              chunks: 'all',
+              name: "vendors",
+              chunks: "all",
             },
           },
         },
       },
     });
     // config.resolve.alias.set('monaco-editor', 'monaco-editor/esm/vs/editor/editor.api.js')
-    config.plugin('monaco-editor').use(MonacoWebpackPlugin, [
+    config.plugin("monaco-editor").use(MonacoWebpackPlugin, [
       {
         languages: [
-          'javascript',
-          'python',
-          'html',
-          'java',
-          'lua',
-          'php',
-          'cpp',
-          'csharp',
-          'shell',
-          'go',
+          "javascript",
+          "python",
+          "html",
+          "java",
+          "lua",
+          "php",
+          "cpp",
+          "csharp",
+          "shell",
+          "go",
         ],
       },
     ]);
     config.module
-      .rule('xlsx')
+      .rule("xlsx")
       .test(/\.(xlsx)$/)
-      .use('file-loader')
-      .loader('file-loader')
+      .use("file-loader")
+      .loader("file-loader")
       .end();
   },
   // mfsu: {},
