9451f1bca4fc81f99413699b53ba931bbbc5e73b
Mario <329988300@qq.com>
2024-11-05 23:09:17 +0800

    翻译

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,6 @@
 import { login } from "@/services/user";
 
 export default function Home() {
-  // todo test
-  login({
-    username: "ryan",
-    password: "123456",
-  });
   return (
     <div>
       <h1>Home</h1>
