effd9d72334f7163fa77f2feafbf6af43e27cbf6
renguanying <ren.yingzi@163.com>
2024-12-03 21:37:02 +0800

    问答部分加上可以结合paperid来作答

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,13 +1,101 @@
-import { ProChat } from '@ant-design/pro-chat';
+import { ProChat, ProChatInstance  } from '@ant-design/pro-chat';
 import {getMessages} from '@/services/question';
 import { useTheme } from 'antd-style';
+import { useEffect, useRef, useState } from 'react';
+import { Dropdown,MenuProps,Space} from 'antd';
+import { useLocation } from 'react-router-dom';
+interface menuType{
+  key:string,
+  label:string
+}
 
 const ChatComponent = () => {
   const theme = useTheme();
+  // const [items,setItems] = useState<MenuProps['items']>([])
+  //ref获取
+  const proChatRef = useRef<ProChatInstance>();
+  //获取url中的paperid参数
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const paperId = params.get('paper_id');
+  console.log("location.search:" + JSON.stringify(location.search) +"     paperId:" + paperId)
+
+  // useEffect(()=>{
+  //   //需要将请求包装成方法
+  //   async function getInitMess(){
+
+  //     const initMessage = await getInitMessages()
+  //     const messages = initMessage.tips.map((item,index)=>{
+  //       return {label:item,key:(index+1).toString()}
+  //     })
+  //     setItems(messages)
+  //   }
+  //   getInitMess()
+  // },[])
+  //给下拉列表添加内容
+  const items: MenuProps['items'] = [
+    {
+      label: '论文的研究问题是什么？',
+      key: '1',
+    },
+    {
+      label: '论文中使用了哪些研究方法？',
+      key: '2',
+    },
+    {
+      label: '论文的主要发现有哪些？',
+      key: '3',
+    },
+    {
+      label: '论文的结论是什么？',
+      key: '4',
+    },
+    {
+      label: '论文的研究结果对相关领域有何意义？',
+      key: '5',
+    },
+    {
+      label: '论文中有哪些重要的理论贡献？',
+      key: '6',
+    },
+    {
+      label: '论文的作者是如何定义研究问题的？',
+      key: '7',
+    },
+    {
+      label: '论文的研究局限性是什么？',
+      key: '8',
+    },
+    {
+      label: '论文中有哪些关键的数据或统计结果？',
+      key: '9',
+    },
+    {
+      label: '论文的研究结果对实践有何启示？',
+      key: '10',
+    },
+  ];
+  //选择下拉列表里的一项，名字只能叫onClick?改了后就会报错
+  const onClick: MenuProps['onClick'] = ({key}) => {
+    console.log("items:" + JSON.stringify(items))
+    console.log("key:" + key)
+    const itemWithKey= items?.find(menuItem => menuItem!.key === key) as menuType;
+    // 如果找到了，打印 label 属性
+    if (itemWithKey) {
+      //message.info(`Click on item ${itemWithKey.label}`);
+      proChatRef.current!.sendMessage(itemWithKey.label);
+
+    } else {
+      console.log('没有找到为key的项');
+    }
+  };
   const handleStream = async (inputMessages) => {
     try {
-      const res = await getMessages(JSON.stringify({ question: inputMessages[inputMessages.length - 1]?.content }))
-      // 确保响应体是ReadableStream
+      // const res = await getMessages(JSON.stringify({ question: inputMessages[inputMessages.length - 1]?.content }))
+      const res = await getMessages(JSON.stringify({ question: inputMessages[inputMessages.length - 1]?.content, 
+        paper_id:[paperId]}))
+      
+      // // 确保响应体是ReadableStream
       if (!res.body) {
         throw new Error('Response body is not a ReadableStream');
       }
@@ -25,7 +113,6 @@ const ChatComponent = () => {
               break;
             }
             const chunkText = decoder.decode(value, { stream: true });
-            console.log("chunkText:" + chunkText)
             let errorFlag = false//记录是否有报错信息
             const newMessages = chunkText.split('\n\n').map((chunk) => {
               const match = chunk.match(/data: (.*)/);
@@ -65,6 +152,26 @@ const ChatComponent = () => {
     <ProChat
       style={{ height: "100%", width: "100%",paddingBottom:"72px",background: theme.colorBgLayout }}
       request={handleStream}
+      chatRef={proChatRef}
+      actions={{
+        render: (defaultDoms) => {
+          return [
+            <Dropdown menu={{ items, onClick}}>
+              <a onClick={(e) => e.preventDefault()}>
+                <Space>
+                  推荐问题参考
+                </Space>
+              </a>
+            </Dropdown>,
+              ...defaultDoms,
+          ];
+        },
+        flexConfig: {
+          gap: 24,
+          direction: 'horizontal',
+          justify: 'space-between',
+        },
+      }}
     />
   );
 };
--- src/services/question.ts
+++ src/services/question.ts
@@ -1,11 +1,19 @@
-import { getLRPrefix} from "@/utils";
-
+import { getLRPrefix, request } from "@/utils";
+import { getToken } from "@/utils/storage";
 export async function getMessages(data:string) {
   return fetch(`${getLRPrefix()}/moonshot/paper_qa`, {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
+        'Authorization':'Bearer ' + getToken(),
     },
     body: data,
+  });
+}
+export async function getInitMessages() {
+  return request(`${getLRPrefix()}/moonshot/paper_qa_init`, {
+    method: "GET",
+    prefix: getLRPrefix(),
+    lrToken: true,
   });
 }
